1cb35157a8739d0da6deb03fa4fb52b753bb0c3b
Domas Trijonis <domas.trijonis@gmail.com>
2025-12-22 19:20:32 +0100

    refactor file reading code

--- 2025/day-1/day-1.go
+++ 2025/day-1/day-1.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strconv"
@@ -13,28 +14,38 @@ type action struct {
 	count int
 }
 
-// Hello returns a greeting for the named person.
+// read each line in a file as an action
 func ReadInput(path string) []action {
 	f, err := os.Open(path)
 	if err != nil {
 		fmt.Printf("error opening file: %v\n", err)
 		os.Exit(1)
 	}
+	defer f.Close()
+
 	actions := make([]action, 0)
+
 	r := bufio.NewReader(f)
-	s, e := r.ReadString('\n')
-	for e == nil {
+	for {
+		s, e := r.ReadString('\n')
+		if len(s) > 0 {
+			right := s[0] == 'R'
+			count, ee := strconv.Atoi(s[1 : len(s)-1]) // exclude line ending
 
-		right := s[0] == 'R'
-		count, ee := strconv.Atoi(s[1 : len(s)-1])
+			if ee != nil {
+				log.Fatal("Failed to parse number, line: ", s, ee)
+			}
 
-		if ee != nil {
-			log.Fatal("Failed to parse number, line: ", s, ee)
+			actions = append(actions, action{count: count, right: right})
 		}
-
-		actions = append(actions, action{count: count, right: right})
-		s, e = r.ReadString('\n')
+		if e == io.EOF {
+			break
+		} else if e != nil {
+			log.Fatal("Fail to read line", e)
+			break
+		}
 	}
+
 	return actions
 }
 
